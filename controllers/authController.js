a01577af664fb3d0c643ff1a97bb6615d0362a2b
sdelrive <sdelrive97@gmail.com>
2022-11-16 18:21:31 -0300

    asd

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,14 @@
 // Configuro el express para que me tire la ayuda de codigo. No es necesario requerirlo.
 const express = require("express");
-// const User = require("../models/User");
+const User = require("../models/User");
 const createUser = async (req, res = express.response) => {
   // me imprime el body de la peticion
   //   console.log(req.body);
 
   const { name, email, password } = req.body;
 
-  // const user = new User(req.body);
-
-  // await user.save();
-
-  console.log("epale");
+  const user = new User(req.body);
+  await user.save();
 
   //   manejo de errores: lo lleve para el middlewre.
 
