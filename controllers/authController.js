8d31e7c85e503cb65cd55898949539e95d9b0707
santiago_del_rivero <santiago.delrivero@cencosud.com>
2022-11-16 21:33:33 -0300

    terminando registro

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,24 +1,37 @@
 // Configuro el express para que me tire la ayuda de codigo. No es necesario requerirlo.
 const express = require("express");
 const User = require("../models/User");
+
 const createUser = async (req, res = express.response) => {
   // me imprime el body de la peticion
   //   console.log(req.body);
 
-  const { name, email, password } = req.body;
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
 
-  const user = new User(req.body);
-  await user.save();
+    if (user) {
+      return res.status(400).json({
+        ok: false,
+        msg: "El usuario ya existe con ese correo",
+      });
+    }
+    user = new User(req.body);
+    await user.save();
 
-  //   manejo de errores: lo lleve para el middlewre.
+    //   manejo de errores: lo lleve para el middlewre.
 
-  res.status(201).json({
-    ok: true,
-    msg: "Registro",
-    name,
-    email,
-    password,
-  });
+    res.status(201).json({
+      ok: true,
+      uid: user.id,
+      name: user.name,
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: "Error.",
+    });
+  }
 };
 
 const loginUser = (req, res) => {
