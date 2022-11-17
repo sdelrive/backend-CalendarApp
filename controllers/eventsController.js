42cd6b6f47fc3ce5ac36d34b1c962925dbf27b6c
sdelrive <sdelrive97@gmail.com>
2022-11-17 18:42:03 -0300

    fin

--- controllers/eventsController.js
+++ controllers/eventsController.js
@@ -38,6 +38,7 @@ const postEvents = async (req, res = express.response) => {
 const putEvents = async (req, res = express.response) => {
   // consigo el id de la url
   const eventID = req.params.id;
+  // el id del usuario
   const uid = req.uid;
   try {
     const event = await Event.findById(eventID);
@@ -81,18 +82,40 @@ const putEvents = async (req, res = express.response) => {
 };
 
 const deleteEvents = async (req, res = express.response) => {
-  const { _id, title, note } = req.body;
-
+  // consigo el id de la url
+  const eventID = req.params.id;
+  // el id del usuario
+  const uid = req.uid;
   try {
+    const event = await Event.findById(eventID);
+
+    if (!event) {
+      return res.status(404).json({
+        ok: false,
+        msg: "Evento no existe con ese id",
+      });
+    }
+
+    // solo el usuario creador del evento puede modificarlo
+    if (event.user.toString() !== uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: "No tiene privilegio para editar este evento",
+      });
+    }
+
+    const deletedEvent = await Event.findByIdAndDelete(eventID);
+
     res.json({
       ok: true,
-      msg: "deleteEvents",
-      _id,
-      title,
-      note,
+      event: deletedEvent,
     });
   } catch (error) {
     console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "no se pibe",
+    });
   }
 };
 
